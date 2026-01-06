ec848694cdc50d4f7e49940babf35669b70c1370
Owusu Robert <kenterobert@gmail.com>
2026-01-06 02:04:16 +0000

    modified: backend/routes/authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
 // routes/authRoutes.js
 import express from 'express';
 import passport from 'passport';
-import { generateToken } from '../config/passport.js';
+import { generateToken } from '../config/passPort.js';
 
 const router = express.Router();
 
